c28c58ac7812bef785d546ac0bbfaeaeeac1fc77
Meelik Kiik <kiik.meelik@gmail.com>
2017-05-03 19:51:55 +0300

    Add navi ui controller placeholder.

---
+++ src/navi-ui.js
@@ -0,0 +1,49 @@
+
+var Tuum = (function(nsp) {
+
+  var data = {
+    // Perception data
+    perc: {
+      data: [
+        [234, 314], [123, 124], [123, 140]
+      ]
+    },
+
+    // Map data
+    map: {
+      node: {
+        id: 123,
+        name: 'Segment #123',
+        landmarks: [
+          [250, 413], [125, 571], [300, 200]
+        ]
+      }
+    },
+
+    // Navigation mesh
+    navmesh: {
+      paths: [
+        {id: 231, data: [[250, 412], [280, 440], [300, 450]]}
+      ]
+    },
+  }
+
+  nsp.NaviSceneController = Class.Extend({
+    init: function(device, scene) {
+      this.dev = device;
+      this.scene = scene;
+
+      this.running = true;
+    },
+
+    process: function() {
+      if(!this.running) return;
+
+      this.device.getPerceptionData().then(function(data) {
+        console.log('[NSC::process]#TODO: ', data);
+      });
+    }
+  });
+
+  return nsp;
+})(Tuum || {});
