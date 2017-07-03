d3f0044f323a4ead1f72dbf225160d4f9b02b8fd
Meelik Kiik <kiik.meelik@gmail.com>
2017-07-03 20:40:01 +0300

    Implement data binding middleware.

--- src/map-ui.js
+++ src/map-ui.js
@@ -1,12 +1,160 @@
 
 var Tuum = (function(nsp) {
 
+  function optional_iterator(input, cb) {
+    if(Array.isArray(input)) {
+      for(var ix in input) cb(input[ix]);
+      return 1;
+    } else cb(input);
+    return 0;
+  }
 
+  var MapUIDataBinder = Class.Extend({
+    init: function(parent) {
+      this._ui = parent;
+      this._ctx = {};
+      this._lod = {};
+      this._z = this._ui.map.getZoom();
+
+      var that = this;
+      google.maps.event.addListener(this._ui.map, 'zoom_changed', function() {
+        var z = that._ui.map.getZoom();
+        if(z == that._z) return;
+
+        if((z < 16) && (that._z >= 16)) {
+          for(var key in that._ctx) {
+            var d = that._ctx[key];
+
+            var res = optional_iterator(d, function(elem) {
+              if('ui' in elem)
+                elem.ui.setMap(null);
+              else
+                elem.setMap(null);
+            });
+
+            if(res == 0) {
+              console.log(d);
+              if(d.lodAnchor) {
+                that._lod[key] = new google.maps.Marker({
+                  position: d.lodAnchor,
+                  label: key,
+                  map: that._ui.map,
+                });
+              }
+            }
+          }
+        } else if((z >= 16) && (that._z < 16)) {
+          for(var key in that._ctx) {
+            var d = that._ctx[key];
+
+            var res = optional_iterator(d, function(elem) {
+              if('ui' in elem)
+                elem.ui.setMap(that._ui.map);
+              else
+                elem.setMap(that._ui.map);
+            });
+
+            if(res == 0) {
+              console.log(that._lod, key)
+              if(key in that._lod) {
+                var e = that._lod[key];
+                e.setMap(null);
+                delete that._lod[key];
+              }
+            }
+          }
+        }
+
+        that._z = z;
+      });
+    },
+    _clear: function(label, data) {
+      if(label in this._ctx) delete this._ctx[label];
+    },
+    polygon: function(label, data) {
+      this._clear(label, data);
+
+      if(!data) return;
+
+      var ui_path = [];
+
+      for(var ix in data) {
+        var d = data[ix];
+        ui_path.push({lat: d[0], lng: d[1]});
+      }
+
+      this._ctx[label] = {
+        ui: new google.maps.Polyline({
+          path: ui_path,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2,
+          map: this._ui.map
+        }),
+        lodAnchor: ui_path[0]
+      };
+
+    },
+    nodes: function(label, data) {
+      this._clear(label, data);
+
+      if(!data) return;
+
+      var out = [];
+
+      for(var ix in data) {
+        var d = data[ix];
+
+        var ui = new google.maps.Marker({
+          position: {lat: d[0], lng: d[1]},
+          icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 2,
+            fillColor: 'red',
+            fillOpacity: 0.8
+          },
+          label: 'S'+ix,
+          map: this._ui.map,
+        });
+
+        out.push(ui);
+      }
+
+      this._ctx[label] = out;
+    },
+    path: function(label, data) {
+      this._clear(label, data);
+
+      if(!data) return;
+
+      var ui_path = [];
+
+      for(var ix in data) {
+        var d = data[ix];
+        ui_path.push({lat: d[0], lng: d[1]});
+      }
+
+      this._ctx[label] = {
+        ui: new google.maps.Polyline({
+          path: ui_path,
+          geodesic: true,
+          strokeColor: '#00FF00',
+          strokeOpacity: 1.0,
+          strokeWeight: 1,
+          map: this._ui.map
+        })
+      };
+
+    },
+  });
 
   var MapUI = Class.Extend({
     init: function(gmap) {
       this.map = gmap;
       this.devs = [];
+
+      this.$ = new MapUIDataBinder(this);
 
       var that = this;
 
@@ -42,6 +190,10 @@ var Tuum = (function(nsp) {
         that.selectedPath.highlight(dat);
       });
     },
+    view: function(p) {
+      this.map.panTo({lat: p[0], lng: p[1]});
+      this.map.setZoom(16);
+    },
 
     markDevice: function(dev, pan) {
       if(typeof pan === 'undefined') pan = false;
@@ -70,7 +222,7 @@ var Tuum = (function(nsp) {
         strokeWeight: 2,
         fillColor: '#FF0000',
         fillOpacity: 0.35,
-        map: map,
+        map: this.map,
         center: {lat: 0, lng: 0},
         radius: 10
       });
